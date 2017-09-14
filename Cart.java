af03c719ca244c644094c8b6d91c4eabc1abd471
snm0911 <31638935+snm0911@users.noreply.github.com>
2017-09-14 15:06:50 +0900

    Add files via upload

---
+++ Cart.java
@@ -0,0 +1,45 @@
+package assignment4;
+
+import java.util.ArrayList;
+
+class Cart {     // 장바구니 클래스
+    ArrayList<Item> list = new ArrayList<Item>();
+    void addItem(String name, int num, int unitPrice) {    
+        list.add(new Item(name, num, unitPrice));
+    }
+    void removeItem(int index) {      
+        list.remove(index);
+    }
+    int getItemNum() {
+        return list.size();
+    }
+    Item getItem(int index) {
+        return list.get(index);
+    }
+    int getTotalPrice() {   
+        int total = 0;
+        for (Item item : list)
+            total += item.getPrice();
+        return total;
+    }
+    void chnageItemNumber(int index, int num) {   
+        Item item = list.get(index);
+        item.num = num;
+    }
+    class Item {
+
+		int num;
+		int unitPrice;
+		String name;
+
+		public Item(String name, int num, int unitPrice) {
+			this.name = name;
+			this.num = num;
+			this.unitPrice = unitPrice;
+		}     // 상품 항목 클래스
+
+		int getPrice() {
+			return num*unitPrice;
+		}
+}
+}
